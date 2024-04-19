773d17e59648ebfcbc6383fd9b7e1a4a75671bb0
Juan Van Heerden <juanivanheer@gmail.com>
2024-04-19 17:16:09 +1000

    feat: created basic navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/store">
+            Store
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </NavbarBs>
+  );
+}
